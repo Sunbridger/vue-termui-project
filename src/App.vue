775d9a8819ec6d4bad004502af9330e030eb37e3
七彩 <wangriqiao@cai-inc.com>
2022-12-11 17:44:24 +0800

    feat: add

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script lang="ts" setup>
+// All imports are automatic but if you want to import anything,
+// remember to import from 'vue-termui':
+// import { ref } from 'vue-termui'
+
+const n = ref(0)
+
+const counter = ref(0)
+onKeyData(['+', 'ArrowRight', 'ArrowUp'], () => {
+  counter.value++
+})
+onKeyData(['-', 'ArrowLeft', 'ArrowDown'], () => {
+  counter.value--
+})
+
+useInterval(() => {
+  n.value++
+}, 600)
+</script>
+
+<template>
+  <Box
+    :padding="1"
+    :margin="1"
+    width="60%"
+    :maxWidth="150"
+    justifyContent="center"
+    alignItems="center"
+    flexDirection="column"
+    borderColor="yellowBright"
+    borderStyle="round"
+  >
+    <Box :marginY="1">
+      <Text color="cyanBright">傻逼啊 World </Text>
+      <Text>{{ n % 2 ? '👋 ' : ' ✋' }}</Text>
+    </Box>
+    <Box>
+      <Text
+        >Counter:
+        <Text :color="counter < 0 ? 'red' : 'green'" bold>{{ counter }}</Text>
+        <Text dimmed>
+          (<Text color="red" bold>-</Text>/<Text bold color="green">+</Text> to
+          change it)</Text
+        >
+      </Text>
+    </Box>
+  </Box>
+</template>
